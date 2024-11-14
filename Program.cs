6e85555d2dd06f3e00218bfaf61e6f352eea52c2
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-14 11:43:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Assignments_2._3
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+                string filePath = "Contact.txt";
+
+                string name = "John Smith";
+                int age = 20;
+                string address = "1st Street";
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.WriteLine("Name: " + name);
+                    writer.WriteLine("Age: " + age);
+                    writer.WriteLine("Address: " + address);
+                }
+
+                Console.WriteLine("Reading details from the file:");
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }           
+        }
+    }
+}
+
